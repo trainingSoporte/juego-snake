84cca8c1ae865afdf019a5125790b7f2463458cb
Adrian Ojeda <pipoadri@gmail.com>
2023-05-18 19:28:22 -0300

    arrowLR

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ console.log(board);
 // squares[index].classList.toggle('snake--body');
 // squares[index + 1].classList.toggle('snake--head');    
 
-index=0;
+index = 0;
 squares[index].classList.add('snake--body');//posicion 0
 squares[++index].classList.add('snake--head'); //posicion 1
 
 
-body.addEventListener('keyup',(e)=>{
+body.addEventListener('keyup', (e) => {
     console.log('keyup');
     console.log(e.key);
     if (e.key === 'ArrowRight') {
@@ -42,9 +42,27 @@ body.addEventListener('keyup',(e)=>{
         }
 
         squares[index].classList.add('snake--body');
-        squares[++index].classList.add('snake--head'); 
+        squares[++index].classList.add('snake--head');
     }
 
-    //! codifica la funcionalidad de movimiento hace la izquierda 
+    //!--codifica la funcionalidad de movimiento hace la izquierda 
+    if (e.key === 'ArrowLeft') {
+        console.log('derecha');
+        if (squares[index - 1].classList.contains('snake--body')) {//0
+            console.log('e');
+            squares[index - 1].classList.remove('snake--body');
+        }
+        if (squares[index].classList.contains('snake--head')) {//1
+            console.log('e');
+            squares[index].classList.remove('snake--head');
+        }
+        if (squares[index + 1].classList.contains('snake--body')) {//0
+            console.log('e');
+            squares[index + 1].classList.remove('snake--body');
+        }
+
+        squares[index].classList.add('snake--body');
+        squares[--index].classList.add('snake--head');
+    }
 })
 
